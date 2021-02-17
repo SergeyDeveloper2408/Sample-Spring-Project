8c940ddff890b87b75b576f9ef26815970d18b7a
Sergey <darthdendroid@gmail.com>
2021-02-17 10:12:19 +0200

    Initial commit

---
+++ src/main/java/com/example/myapplication/repositories/MessageRepository.java
@@ -0,0 +1,10 @@
+package com.example.myapplication.repositories;
+
+import com.example.myapplication.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
---
+++ src/main/java/com/example/myapplication/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.myapplication.repositories;
+
+import com.example.myapplication.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    User findByActivationCode(String code);
+}
+
